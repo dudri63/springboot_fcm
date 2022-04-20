41a1b5882721a2c9084329bd46b71f7af6c75064
dudri63 <dudri63@naver.com>
2022-04-20 23:21:02 +0900

    add firebase_service_key.json

---
+++ src/main/java/com/example/springboot_fcm/SpringbootFcmApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_fcm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFcmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootFcmApplication.class, args);
+    }
+
+}
